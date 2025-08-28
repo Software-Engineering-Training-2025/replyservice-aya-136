e868305e6acafc30ae809141c83a1a3f92ab1444
aya-136 <ayazeyadlolo@gmail.com>
2025-08-28 19:23:24 +0300

    submit try 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        ReplyService bot = new ReplyService();
+
+        System.out.println(bot.reply("hi", ReplyType.FORMAL));
+        System.out.println(bot.reply("hello", ReplyType.FRIENDLY));
+        System.out.println(bot.reply("how are you", ReplyType.CONCISE));
+        System.out.println(bot.reply("unknown message", ReplyType.FORMAL));
+        System.out.println(bot.reply("", ReplyType.FRIENDLY));
     }
 }
